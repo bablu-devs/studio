fae485954c26305345008728808953acb849ca24
Md Bablu <bablubabu664@gmail.com>
2026-01-19 14:47:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6123005816-2df72",
+  "appId": "1:614945405455:web:fa8dd7f862c6bd03ab417a",
+  "apiKey": "AIzaSyAG6OOXvWVPS3W-FhzSRIx3oFRCtjCauj0",
+  "authDomain": "studio-6123005816-2df72.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "614945405455"
+};
